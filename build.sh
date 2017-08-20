6de5d94015f781b05df29934cfe5f018b806c725
Vlvxingze <1207223090@qq.com>
2017-08-20 20:30:48 +0800

    瞎玩：测试还原

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-#!/bin/bash
 
-chmod +x ./package.sh
-chmod +x ./prepare-build.sh
 ./prepare-build.sh && mvn clean install && ./package.sh
--- prepare-build.sh
+++ prepare-build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-chmod +x ./remap.sh
-chmod +x ./decompile.sh
-chmod +x ./init.sh
-chmod +x ./applyPatches.sh
-
 git submodule update --init && ./remap.sh && ./decompile.sh && ./init.sh && ./applyPatches.sh
 
 # Generate paperclip jar in this stage
