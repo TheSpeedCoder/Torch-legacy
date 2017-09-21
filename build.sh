cbf65d5eafbfdd2093c111529cdd3493a1a1025e
Joseph Robinson <smartech56@gmail.com>
2017-09-21 10:21:40 -0700

    added install dependency maven

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #!/bin/bash
 sudo apt update && sudo apt upgrade
+sudo apt install maven
 chmod +x ./package.sh
 chmod +x ./prepare-build.sh
 ./prepare-build.sh && mvn clean install && ./package.sh
