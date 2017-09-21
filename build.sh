6c162b4fb52ee4b67f380689287153647b891412
Joseph Robinson <smartech56@gmail.com>
2017-09-21 12:28:30 -0700

    build v2

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #!/bin/bash
+echo Getting/Updating dependencies...
 sudo apt update && sudo apt upgrade
 sudo apt install maven
+echo checking environment...
+java -version
+mvn -version
+echo building...
 chmod +x ./package.sh
 chmod +x ./prepare-build.sh
 ./prepare-build.sh && mvn clean install && ./package.sh
